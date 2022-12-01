63091fb1fbd48355c5c2edb09cca7b56a4b5375b
Juliano <julianofirme23@gmail.com>
2022-12-01 19:42:42 -0300

    feat: create the user model

---
+++ models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	FirstName string         `json:"firstName"`
+	LastName  string         `json:"lastName"`
+	Username  string         `json:"username"`
+	Email     string         `json:"email"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"created"`
+	UpdatedAt time.Time      `json:"updated"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted"`
+}

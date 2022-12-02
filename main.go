63646f5820135a01cdb34cb8e51dbc2db923c7ed
Juliano <julianofirme23@gmail.com>
2022-12-01 21:09:49 -0300

    feat: call the StartDB  func on main

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/jfirme-sys/go-auth-docker.git/database"
 	"github.com/jfirme-sys/go-auth-docker.git/server"
 )
 
 func main() {
+	database.StartDB()
+
 	server := server.NewServer()
 
 	server.Run()
